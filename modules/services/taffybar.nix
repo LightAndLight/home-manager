33fad3ddf249a8f79f4640d95bb2104617bf063a
Isaac Elliott <isaace71295@gmail.com>
2020-08-28 07:02:08 +1000

    This is a combination of 2 commits.
    taffybar: allow custom taffybar.hs

--- modules/services/taffybar.nix
+++ modules/services/taffybar.nix
@@ -20,25 +20,47 @@ in {
         example = literalExample "pkgs.taffybar";
         description = "The package to use for the Taffybar binary.";
       };
+
+      config = mkOption {
+        default = null;
+        defaultText = literalExample "null";
+        type = types.nullOr types.path;
+        example = literalExample "./files/taffybar.hs";
+        description = "The file to use for the Taffybar config.";
+      };
     };
   };
 
-  config = mkIf config.services.taffybar.enable {
-    systemd.user.services.taffybar = {
-      Unit = {
-        Description = "Taffybar desktop bar";
-        After = [ "graphical-session-pre.target" ];
-        PartOf = [ "graphical-session.target" ];
-      };
+  config = mkIf config.services.taffybar.enable (
+    mkMerge [
+      {
+        systemd.user.services.taffybar = {
+          Unit = {
+            Description = "Taffybar desktop bar";
+            After = [ "graphical-session-pre.target" ];
+            PartOf = [ "graphical-session.target" ];
+          };
 
-      Service = {
-        ExecStart = "${cfg.package}/bin/taffybar";
-        Restart = "on-failure";
-      };
+          Service = {
+            ExecStart = "${cfg.package}/bin/taffybar";
+            Restart = "on-failure";
+          };
 
-      Install = { WantedBy = [ "graphical-session.target" ]; };
-    };
+          Install = { WantedBy = [ "graphical-session.target" ]; };
+        };
 
-    xsession.importedVariables = [ "GDK_PIXBUF_MODULE_FILE" ];
-  };
+        xsession.importedVariables = [ "GDK_PIXBUF_MODULE_FILE" ];
+      }
+
+      (mkIf (cfg.config != null) {
+        systemd.user.services.taffybar.Unit.X-Restart-Triggers = [
+          "${config.xdg.configFile."taffybar/taffybar.hs".source}"
+        ];
+        xdg.configFile."taffybar/taffybar.hs" = {
+          source = cfg.config;
+          onChange = "systemctl --user restart taffybar.service";
+        };
+      })
+    ]
+  );
 }
